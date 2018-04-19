0e88a0958be98a2a67bc73c54a3b79fea25fbcd7
nicolas-m-gomez <gomez.nicolas.martin@gmail.com>
2018-04-19 15:43:00 -0300

    Dominio - clase Usuario

---
+++ src/xpressutn/modelo/Usuario.java
@@ -0,0 +1,88 @@
+package xpressutn.modelo;
+
+import java.util.List;
+
+import xpressutn.annotations.Column;
+import xpressutn.annotations.Id;
+import xpressutn.annotations.ManyToOne;
+import xpressutn.annotations.OneToMany;
+import xpressutn.annotations.Table;
+
+@Table(name = "XPRESS.USUARIO")
+public class Usuario
+{
+	@Id(strategy = Id.IDENTITY)
+	@Column(name = "id_usuario")
+	private int idUsuario;
+	
+	@Column
+	private String username;
+	
+	@Column
+	private String password;
+	
+	@ManyToOne(columnName = "id_persona", fetchType = ManyToOne.EAGER)
+	private Persona persona;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<UsuarioRol> roles;
+	
+	public Usuario (){}
+
+	public int getIdUsuario()
+	{
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario)
+	{
+		this.idUsuario=idUsuario;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username=username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password=password;
+	}
+
+	public Persona getPersona()
+	{
+		return persona;
+	}
+
+	public void setPersona(Persona persona)
+	{
+		this.persona=persona;
+	}
+
+	public List<UsuarioRol> getRoles()
+	{
+		return roles;
+	}
+
+	public void setRoles(List<UsuarioRol> roles)
+	{
+		this.roles=roles;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Usuario [idUsuario="+idUsuario+", username="+username+", password="+password+"]";
+	}
+	
+}
