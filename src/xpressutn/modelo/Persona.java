19cf9f9a572a08698ee549013591f7432eaff35a
nicolas-m-gomez <gomez.nicolas.martin@gmail.com>
2018-04-19 15:41:04 -0300

    Dominio - clase Persona

--- src/xpressutn/modelo/Persona.java
+++ src/xpressutn/modelo/Persona.java
@@ -1,12 +1,14 @@
 package xpressutn.modelo;
 
 import java.util.Date;
+import java.util.List;
 
 import xpressutn.annotations.Column;
 import xpressutn.annotations.Id;
+import xpressutn.annotations.OneToMany;
 import xpressutn.annotations.Table;
 
-@Table(name="PERSONA")
+@Table(name="XPRESS.PERSONA")
 public class Persona
 {
 	@Id(strategy=Id.IDENTITY) 
@@ -22,6 +24,56 @@ public class Persona
 	@Column(name="fecha_alta")
 	private Date fechaAlta;
 	
-	public Persona(){}
+	@OneToMany(mappedBy = "id_persona")
+	private List<Usuario> usuarios;
 	
+	public Persona(){}
+
+	public int getIdPersona()
+	{
+		return idPersona;
+	}
+
+	public void setIdPersona(int idPersona)
+	{
+		this.idPersona=idPersona;
+	}
+
+	public String getNombre()
+	{
+		return nombre;
+	}
+
+	public void setNombre(String nombre)
+	{
+		this.nombre=nombre;
+	}
+
+	public String getDireccion()
+	{
+		return direccion;
+	}
+
+	public void setDireccion(String direccion)
+	{
+		this.direccion=direccion;
+	}
+
+	public Date getFechaAlta()
+	{
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Date fechaAlta)
+	{
+		this.fechaAlta=fechaAlta;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Persona [idPersona="+idPersona+", nombre="+nombre+", direccion="+direccion+", fechaAlta="+fechaAlta+"]";
+	}
+
+
 }
