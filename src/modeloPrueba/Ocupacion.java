d24f79ffe54a91993d509eb0c77bbd0e73b47ec8
Matias Llampa <hector.llampa@gmail.com>
2018-07-07 23:28:20 -0300

    [UPDATE] Clases del modelo de prueba

---
+++ src/modeloPrueba/Ocupacion.java
@@ -0,0 +1,77 @@
+package modeloPrueba;
+
+import java.util.List;
+
+import xpressutn.annotations.*;;
+
+@Table(name="ocupacion")
+public class Ocupacion
+{
+	@Id(strategy=Id.IDENTITY)
+	@Column(name="id_ocupacion")
+	private Integer idOcupacion;
+	
+	@Column(name="descripcion")
+	private String descripcion;
+	
+	@ManyToOne(columnName="id_tipoocupacion",fetchType=ManyToOne.LAZY)
+	public TipoOcupacion tipoOcupacion;
+
+	@OneToMany(mappedBy="id_ocupacion")
+	public List<Persona> personas;
+	
+	public List<Persona> getPersonas()
+	{
+		return personas;
+	}
+
+	public void setPersonas(List<Persona> personas)
+	{
+		this.personas=personas;
+	}
+
+	public Integer getIdOcupacion()
+	{
+		return idOcupacion;
+	}
+
+	public void setIdOcupacion(Integer idOcupacion)
+	{
+		this.idOcupacion=idOcupacion;
+	}
+
+	public String getDescripcion()
+	{
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion)
+	{
+		this.descripcion=descripcion;
+	}
+
+	public TipoOcupacion getTipoOcupacion()
+	{
+		return tipoOcupacion;
+	}
+
+	public void setTipoOcupacion(TipoOcupacion tipoOcupacion)
+	{
+		this.tipoOcupacion=tipoOcupacion;
+	}
+
+	@Override
+	public String toString()
+	{
+		return getDescripcion();
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		Ocupacion other = (Ocupacion)o;
+		return other.getIdOcupacion().equals(idOcupacion)
+			&& other.getDescripcion().equals(getDescripcion())
+			&& other.getTipoOcupacion().equals(getTipoOcupacion());
+	}	
+}
