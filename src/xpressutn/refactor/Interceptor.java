5b40a09800f4c2b31c6fa50680204efc79729ffa
rojascristian <rojas.r.cristian@gmail.com>
2018-07-15 20:39:55 -0300

    [UPDATE] Refactor methodInterceptor

--- src/xpressutn/refactor/Interceptor.java
+++ src/xpressutn/refactor/Interceptor.java
@@ -4,45 +4,77 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
-import java.util.LinkedHashMap;
+import java.util.List;
 
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import xpressutn.annotations.OneToMany;
+import xpressutn.utils.MetaData;
 
 public class Interceptor implements MethodInterceptor
 {
 
 	private Object target;
-	LinkedHashMap<String,Field> oneToManyColumnsField;
+	MetaData metaData;
 
-	public Interceptor(Object target, LinkedHashMap<String,Field> oneToManyColumnsField){
+	public Interceptor(Object target, MetaData metadataEntry){
 		this.target=target;
-		this.oneToManyColumnsField = oneToManyColumnsField;
+		this.metaData = metadataEntry;
 	}
 	
 	@Override
 	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable
 	{
-		if(this.oneToManyColumnsField.containsKey(method.getName().toLowerCase()))
+		if(this.metaData.getOneToManyColumnsField().containsKey(method.getName().toLowerCase()))
 		{
 			System.out.println("método del proxy.\n");
-			Field field=this.oneToManyColumnsField.get(method.getName().toLowerCase());
+			Field field=this.metaData.getOneToManyColumnsField().get(method.getName().toLowerCase());
 			Class clase=getClassParametized(field);
 
 			String xql=generateQuerySuperPowa(method);
 			System.out.println("PSEUDO QUERY GENERADA QUE SE LE PASA AL MÉTODO QUERY DE XPRESS");
 			System.out.println(xql+"\n");
-			method.invoke(obj,XpressUTN.query(clase,xql,1));
-			return null;
+			
+			Method setterMethod = getMethodSetter(method);
+			Method getterMethodPK = getMethodPK();
+			List objectList = XpressUTN.query(clase,xql,getterMethodPK.invoke(obj));
+//			setterMethod.invoke(obj,objectList);
+			return objectList;
 		}
 		System.out.println("método de la clase.");
+		System.out.println(method.getName());
 		return proxy.invoke(target,args);
 	}
 	
+	private Method getMethodPK()
+	{
+		Method setterMethod = null;
+		Field fieldPK = metaData.getPrimaryKeyField();
+		for(Method metodo: this.metaData.getMetodos()){
+			if(metodo.getName().equalsIgnoreCase("get"+fieldPK.getName())){
+				setterMethod = metodo;
+				break;
+			}
+		}
+		return setterMethod;
+	}
+
+	private Method getMethodSetter(Method method)
+	{
+		String strMethod = method.getName().substring(3,method.getName().length());
+		Method getterMethod = null;
+		for(Method metodo: this.metaData.getMetodos()){
+			if(metodo.getName().equalsIgnoreCase("set"+strMethod)){
+				getterMethod = metodo;
+				break;
+			}
+		}
+		return getterMethod;
+	}
+
 	private String generateQuerySuperPowa(Method method)
 	{
-		Field field=this.oneToManyColumnsField.get(method.getName().toLowerCase());
+		Field field=this.metaData.getOneToManyColumnsField().get(method.getName().toLowerCase());
 		Class clase=getClassParametized(field);
 		Annotation anotacionObtenida=field.getAnnotation(OneToMany.class);
 		String queryStr="SELECT * FROM "+clase.getSimpleName()+" x WHERE x."+((OneToMany)anotacionObtenida).mappedBy()+" = ?";
