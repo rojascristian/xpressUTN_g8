07d8e631d82339a348784e3744fa9fcfcaf104e5
rojascristian <rojas.r.cristian@gmail.com>
2018-07-16 00:12:36 -0300

    [UPDATE] Generacion de la query dependiendo si es manytoone(lazy) o onetomany

--- src/xpressutn/refactor/Interceptor.java
+++ src/xpressutn/refactor/Interceptor.java
@@ -4,10 +4,13 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
+import xpressutn.annotations.ManyToOne;
 import xpressutn.annotations.OneToMany;
 import xpressutn.utils.MetaData;
 
@@ -16,19 +19,23 @@ public class Interceptor implements MethodInterceptor
 
 	private Object target;
 	MetaData metaData;
+	private LinkedHashMap<String,MetaData> metaDataHM;
 
-	public Interceptor(Object target, MetaData metadataEntry){
+	public Interceptor(Object target, MetaData metadataEntry, LinkedHashMap<String,MetaData> metadataHM){
 		this.target=target;
 		this.metaData = metadataEntry;
+		this.metaDataHM = metadataHM;
 	}
 	
 	@Override
 	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable
 	{
-		if(this.metaData.getOneToManyColumnsField().containsKey(method.getName().toLowerCase()))
+		if(this.metaData.getLazyFields().containsKey(method.getName().toLowerCase()))
+//			if(this.metaData.getOneToManyColumnsField().containsKey(method.getName().toLowerCase()))
 		{
 			System.out.println("método del proxy.\n");
-			Field field=this.metaData.getOneToManyColumnsField().get(method.getName().toLowerCase());
+//			Field field=this.metaData.getOneToManyColumnsField().get(method.getName().toLowerCase());
+			Field field=this.metaData.getLazyFields().get(method.getName().toLowerCase());
 			Class clase=getClassParametized(field);
 
 			String xql=generateQuerySuperPowa(method);
@@ -37,9 +44,16 @@ public class Interceptor implements MethodInterceptor
 			
 			Method setterMethod = getMethodSetter(method);
 			Method getterMethodPK = getMethodPK();
-			List objectList = XpressUTN.query(clase,xql,getterMethodPK.invoke(obj));
+			if(field.isAnnotationPresent(OneToMany.class)){
+				return XpressUTN.query(clase,xql,getterMethodPK.invoke(obj));
+			} else if(field.isAnnotationPresent(ManyToOne.class)){
+//				TODO: OBTENER LA SENTENCIA SQL QUE TENGA TODOS LOS CAMPOS(sean o no lazy)
+				String str = XpressUTN.generateExplicitQuery(getterMethodPK.invoke(obj),this.metaDataHM, this.metaData);
+				System.out.println(str);
+//				XpressUTN.query(this.metaData.getClase(),xql,getterMethodPK.invoke(obj));
+			}
 //			setterMethod.invoke(obj,objectList);
-			return objectList;
+			return null;
 		}
 		System.out.println("método de la clase.");
 		System.out.println(method.getName());
@@ -74,18 +88,31 @@ public class Interceptor implements MethodInterceptor
 
 	private String generateQuerySuperPowa(Method method)
 	{
-		Field field=this.metaData.getOneToManyColumnsField().get(method.getName().toLowerCase());
+//		Field field=this.metaData.getOneToManyColumnsField().get(method.getName().toLowerCase());
+		Field field=this.metaData.getLazyFields().get(method.getName().toLowerCase());
 		Class clase=getClassParametized(field);
-		Annotation anotacionObtenida=field.getAnnotation(OneToMany.class);
-		String queryStr="SELECT * FROM "+clase.getSimpleName()+" x WHERE x."+((OneToMany)anotacionObtenida).mappedBy()+" = ?";
+		String queryStr = null;
+		if(field.isAnnotationPresent(OneToMany.class)){
+			Annotation anotacionObtenida=field.getAnnotation(OneToMany.class);
+			queryStr="SELECT * FROM "+clase.getSimpleName()+" x WHERE x."+((OneToMany)anotacionObtenida).mappedBy()+" = ?";
+		}
+		if(field.isAnnotationPresent(ManyToOne.class)){
+			queryStr="SELECT * FROM "+this.metaData.getClase().getName()+" x WHERE x."+this.metaData.getPrimaryKeyField().getName()+" = ?";			
+		}
 		return queryStr;
 	}
 
 	private Class getClassParametized(Field field)
 	{
-		ParameterizedType stringListType=(ParameterizedType)field.getGenericType();
-		Class<?> stringListClass=(Class<?>)stringListType.getActualTypeArguments()[0];
-		return stringListClass;
+		if(field.isAnnotationPresent(ManyToOne.class)){
+			return field.getType();
+		}
+		if(field.isAnnotationPresent(OneToMany.class)){
+			ParameterizedType stringListType=(ParameterizedType)field.getGenericType();
+			Class<?> stringListClass=(Class<?>)stringListType.getActualTypeArguments()[0];
+			return stringListClass;
+		}
+		return null;
 	}
 
 
